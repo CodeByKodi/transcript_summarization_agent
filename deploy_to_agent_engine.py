9189c0881139b3002f7eafd189825fccbd672ea0
CodeWizard0519_rsa <kodeeswaran.malaimuthu@gmail.com>
2025-07-22 18:58:47 +0530

    Credientials

--- deploy_to_agent_engine.py
+++ deploy_to_agent_engine.py
@@ -3,13 +3,19 @@ import vertexai
 from vertexai import agent_engines
 from dotenv import load_dotenv
 from agent import root_agent
+from google.oauth2 import service_account
 
 load_dotenv()
+
+credentials = service_account.Credentials.from_service_account_file(
+    "service-account.json"
+)
 
 vertexai.init(
     project=os.getenv("GOOGLE_CLOUD_PROJECT"),
     location=os.getenv("GOOGLE_CLOUD_LOCATION"),
     staging_bucket="gs://geni-project_cloudbuild",
+    credentials=credentials,
 )
 
 remote_app = agent_engines.create(
