2b0fe1a4c20b955d98fc21468e3433bdd2177b93
CodeWizard0519_rsa <kodeeswaran.malaimuthu@gmail.com>
2025-07-22 18:35:31 +0530

    Fix

--- agent_engine_utils.py
+++ agent_engine_utils.py
@@ -8,7 +8,7 @@ load_dotenv()
 vertexai.init(
     project=os.getenv("GOOGLE_CLOUD_PROJECT"),
     location=os.getenv("GOOGLE_CLOUD_LOCATION"),
-    staging_bucket="gs://" + os.getenv("GOOGLE_CLOUD_PROJECT") + "-bucket",
+    staging_bucket=os.getenv("STAGING_BUCKET")
 )
 
 def list():
